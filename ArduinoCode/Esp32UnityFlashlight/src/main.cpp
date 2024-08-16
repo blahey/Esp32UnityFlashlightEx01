98050ba6e95b7611424a50b023389ab097ee57f4
Byron Lahey <Byron.Lahey@asu.edu>
2024-08-15 20:57:36 -0700

    First Commit

---
+++ ArduinoCode/Esp32UnityFlashlight/src/main.cpp
@@ -0,0 +1,144 @@
+#include <Arduino.h>
+
+#include <Bounce2.h>
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#include <elapsedMillis.h>
+
+// Bounce setup
+const int FLASHLIGHT_BUTTON_PIN = 26;
+Bounce flashlightButton = Bounce(); // Instantiate a Bounce object
+
+// Replace with your network credentials and target IP
+const char* ssid = "entanglement";
+const char* password = "BU@37ajMVAg2";
+const char* udpAddress = "192.168.0.8"; // Replace with your Unity IP address
+const int udpPort = 4210;
+const int receivePort = 4211;
+
+WiFiUDP udp;
+WiFiUDP receiveUdp;
+int sensorValue32 = 0;
+int sensorValue33 = 0;
+int sensorValue34 = 0;
+int sensorValue35 = 0;
+int flashlightButtonState = 1;
+int rssi = 0;
+
+// Timers for intervals
+elapsedMillis sensorValueTimer;
+elapsedMillis rssiValueTimer;
+
+// Intervals in milliseconds
+const unsigned long sensorValueInterval = 40;
+const unsigned long rssiValueInterval = 5000;
+
+float posX, posY, posZ;
+
+void sendSensorData() {
+  // Create a JSON document and populate it with sensor data
+  DynamicJsonDocument doc(1024);
+  doc["sensorValue32"] = sensorValue32;
+  doc["sensorValue33"] = sensorValue33;
+  doc["sensorValue34"] = sensorValue34;
+  doc["sensorValue35"] = sensorValue35;
+  doc["flashlightButtonState"] = flashlightButtonState;
+  doc["rssi"] = rssi;
+
+  // Serialize JSON document to a string
+  char output[256];
+  size_t len = serializeJson(doc, output);
+
+  // Send the JSON string via UDP
+  udp.beginPacket(udpAddress, udpPort);
+  udp.write((uint8_t*)output, len);
+  udp.endPacket();
+
+  // Debugging: print the sent data to the Serial Monitor
+  Serial.println("Data sent: " + String(output));
+}
+
+void receivePositionData() {
+  int packetSize = receiveUdp.parsePacket();
+  if (packetSize) {
+    char incomingPacket[255];
+    int len = receiveUdp.read(incomingPacket, 255);
+    if (len > 0) {
+      incomingPacket[len] = 0;
+    }
+    Serial.printf("Received packet of size %d: %s\n", len, incomingPacket);
+
+    // Parse the incoming JSON data
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, incomingPacket);
+    if (!error) {
+      posX = doc["x"];
+      posY = doc["y"];
+      posZ = doc["z"];
+      Serial.printf("Received position data - X: %f, Y: %f, Z: %f\n", posX, posY, posZ);
+    } else {
+      Serial.print("Failed to parse JSON: ");
+      Serial.println(error.c_str());
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  // Wait for the connection to the Wi-Fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Start UDP
+  receiveUdp.begin(receivePort);
+  pinMode(26, INPUT);
+
+  //Bounce setup
+  flashlightButton.attach(FLASHLIGHT_BUTTON_PIN, INPUT);
+  flashlightButton.interval(25); 
+
+}
+
+void loop() {
+  // Update sensorValues at the specified interval
+  if (sensorValueTimer >= sensorValueInterval) {
+    sensorValue32 = analogRead(32); // Read from analog pin 32
+    sensorValue33 = analogRead(33); // Read from analog pin 33
+    sensorValue34 = analogRead(34); // Read from analog pin 34
+    sensorValue35 = analogRead(35); // Read from analog pin 35
+
+    flashlightButton.update(); //Update the Bounce instance
+    if(flashlightButton.fell() ) {
+      flashlightButtonState = 0; // Send a 0 if the button was pressed (transition from HIGH to LOW)
+    }
+    else {
+      flashlightButtonState = 1; // Send a 1 if the button state did not transition fron HIGH to LOW
+    }
+    //switchValue26 = digitalRead(26); // Read from digital pin 26
+    sensorValueTimer = 0; // Reset the timer
+  }
+
+  // Update rssiValue at the specified interval
+  if (rssiValueTimer >= rssiValueInterval) {
+    rssi = WiFi.RSSI();
+    rssiValueTimer = 0; // Reset the timer
+  }
+
+  // Send sensor data via UDP
+  sendSensorData();
+
+  // Receive position data from Unity
+  receivePositionData();
+
+}
+
+
+
+
---
+++ myScripts/DataIO.cs
@@ -0,0 +1,115 @@
+// comment
+// new comment in practiceBrancingMerging branch
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class DataIO : MonoBehaviour
+{
+    private UdpClient udpClient;
+    private UdpClient sendUdpClient;
+    public string esp32IP = "192.168.0.25"; // Replace with your ESP32 IP address
+    public int receivePort = 4210; // Port number for receiving data
+    public int sendPort = 4211; // Port number for sending data
+    public GameObject targetObject; // The game object to track
+
+    public int sensorValue32; // Sensor data from pin 32
+    public int sensorValue33; // Sensor data from pin 33
+    public int sensorValue34; // Sensor data from pin 34
+    public int sensorValue35; // Sensor data from pin 35
+    public int flashlightButtonState; // Switch data from pin 26  
+    public int rssiValue; // WiFi signal strength (RSSI)
+
+    [System.Serializable]
+    public class SensorData
+    {
+        public int sensorValue32;
+        public int sensorValue33;
+        public int sensorValue34;
+        public int sensorValue35;
+        public int flashlightButtonState;
+        public int rssi;
+    }
+
+    [System.Serializable]
+    public class PositionData
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    void Start()
+    {
+        udpClient = new UdpClient(receivePort);
+        udpClient.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+        sendUdpClient = new UdpClient();
+        InvokeRepeating("SendPositionData", 0f, 0.04f); // Send position data every 40ms
+    }
+
+    void ReceiveCallback(IAsyncResult ar)
+    {
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, receivePort);
+        byte[] receiveBytes = udpClient.EndReceive(ar, ref endPoint);
+        string dataFromESP32 = Encoding.ASCII.GetString(receiveBytes);
+        Debug.Log("Data from ESP32: " + dataFromESP32);
+
+        try
+        {
+            SensorData data = JsonUtility.FromJson<SensorData>(dataFromESP32);
+            sensorValue32 = data.sensorValue32;
+            sensorValue33 = data.sensorValue33;
+            sensorValue34 = data.sensorValue34;
+            sensorValue35 = data.sensorValue35;
+            flashlightButtonState = data.flashlightButtonState;
+            rssiValue = data.rssi;
+            Debug.Log("Parsed Sensor Value 32: " + sensorValue32);
+            Debug.Log("Parsed Sensor Value 33: " + sensorValue33);
+            Debug.Log("Parsed Sensor Value 34: " + sensorValue34);
+            Debug.Log("Parsed Sensor Value 35: " + sensorValue35);
+            Debug.Log("Parsed flashlightButtonState: " + flashlightButtonState);
+            Debug.Log("Parsed RSSI Value: " + rssiValue);
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Failed to parse JSON: " + e.Message);
+        }
+
+        // Restart receiving data
+        udpClient.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+    }
+
+    void SendPositionData()
+    {
+        if (targetObject != null)
+        {
+            Vector3 position = targetObject.transform.position;
+            PositionData positionData = new PositionData
+            {
+                x = position.x,
+                y = position.y,
+                z = position.z
+            };
+
+            string json = JsonUtility.ToJson(positionData);
+            byte[] data = Encoding.ASCII.GetBytes(json);
+            sendUdpClient.Send(data, data.Length, esp32IP, sendPort);
+            Debug.Log("Position Data sent: " + json);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (udpClient != null)
+        {
+            udpClient.Close();
+        }
+        if (sendUdpClient != null)
+        {
+            sendUdpClient.Close();
+        }
+    }
+}

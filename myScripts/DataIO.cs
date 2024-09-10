a11b5c49cae3682cc3d7f013e69605851243b610
Byron Lahey <Byron.Lahey@asu.edu>
2024-09-09 20:30:50 -0700

    Corrected prior changes that resulted in incomplete code.

--- myScripts/DataIO.cs
+++ myScripts/DataIO.cs
@@ -1,3 +1,55 @@
+// comment
+// new comment in practiceBrancingMerging branch
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class DataIO : MonoBehaviour
+{
+    private UdpClient udpClient;
+    private UdpClient sendUdpClient;
+    public string esp32IP = "192.168.0.25"; // Replace with your ESP32 IP address
+    public int receivePort = 4210; // Port number for receiving data
+    public int sendPort = 4211; // Port number for sending data
+    public GameObject targetObject; // The game object to track
+
+    public int sensorValue32; // Sensor data from pin 32
+    public int sensorValue33; // Sensor data from pin 33
+    public int sensorValue34; // Sensor data from pin 34
+    public int sensorValue35; // Sensor data from pin 35
+    public int flashlightButtonState; // Switch data from pin 26  
+    public int rssiValue; // WiFi signal strength (RSSI)
+
+    [System.Serializable]
+    public class SensorData
+    {
+        public int sensorValue32;
+        public int sensorValue33;
+        public int sensorValue34;
+        public int sensorValue35;
+        public int flashlightButtonState;
+        public int rssi;
+    }
+
+    [System.Serializable]
+    public class PositionData
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    void Start()
+    {
+        udpClient = new UdpClient(receivePort);
+        udpClient.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+        sendUdpClient = new UdpClient();
+        InvokeRepeating("SendPositionData", 0f, 0.04f); // Send position data every 40ms
+    }
+
 void ReceiveCallback(IAsyncResult ar)
 {
     IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, receivePort);
@@ -39,5 +91,19 @@ void SendPositionData()
         byte[] data = Encoding.ASCII.GetBytes(positionData);
         sendUdpClient.Send(data, data.Length, esp32IP, sendPort);
         Debug.Log("Position Data sent: " + positionData);
+    }
+}
+
+
+    void OnApplicationQuit()
+    {
+        if (udpClient != null)
+        {
+            udpClient.Close();
+        }
+        if (sendUdpClient != null)
+        {
+            sendUdpClient.Close();
+        }
     }
 }
